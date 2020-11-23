f6d67fdd2f18e0a56d026281e30c20b772e7c159
linhaisongtao321 <1129024987@qq.com>
2020-11-23 10:35:38 +0800

    20200023添加

--- src/main/java/com/lhst/springboot_project/mapper/SysLogDao.java
+++ src/main/java/com/lhst/springboot_project/mapper/SysLogDao.java
@@ -1,6 +1,6 @@
 package com.lhst.springboot_project.mapper;
 
-import com.lhst.springboot_project.entity.SysLogEntity;
+import com.lhst.springboot_project.po.SysLogEntity;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
