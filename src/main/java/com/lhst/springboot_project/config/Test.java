7547f7474aee1fdafb4ddf9c87771df373c6b63e
linhaisongtao321 <1129024987@qq.com>
2022-03-22 14:46:33 +0800

    合并冲突

--- src/main/java/com/lhst/springboot_project/config/Test.java
+++ src/main/java/com/lhst/springboot_project/config/Test.java
@@ -3,6 +3,8 @@ package com.lhst.springboot_project.config;
 public class Test {
     public static void main(String[] args) {
         System.out.println("test main");
+
+        System.out.println("hot-fix 修改");
         System.out.println("回到master");
     }
 }
