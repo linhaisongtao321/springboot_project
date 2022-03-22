20077e82d11c3ff42146e2b2221f1a2e77b1579e
linhaisongtao321 <1129024987@qq.com>
2022-03-22 14:41:16 +0800

    master  修改2

--- src/main/java/com/lhst/springboot_project/config/Test.java
+++ src/main/java/com/lhst/springboot_project/config/Test.java
@@ -3,5 +3,6 @@ package com.lhst.springboot_project.config;
 public class Test {
     public static void main(String[] args) {
         System.out.println("test main");
+        System.out.println("回到master");
     }
 }
